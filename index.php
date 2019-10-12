82c040be8d67e012d387023d20ea278ce1de401d
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2019-10-12 20:25:14 +0300

    Create index.php
    Test

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype HTML>
+<html>
+<head>
+  <title>
+    Приветствуем на сайте
+  </title>
+</head>
+<body>
+  <header>
+    Тестовая страница
+  </header>
+  <main>
+  </main>
+  <footer>
+  </footer>
+</body>
+</html>
