8a75a597b715d23cadd8a023dea999bbd96b6ede
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2019-10-12 22:24:18 +0300

    func +

--- webpack/src/som.js
+++ webpack/src/som.js
@@ -2,9 +2,9 @@ function sum(...numbers)
 {
   let sum = 0;
 
-  for(let i=0; i < len(numbers); i++)
+  for(let i=0; i < numbers.length; i++)
   {
-    sum += numbers[i]
+    sum += numbers[i];
   }
 }
 
